32f6c963c9ce16ffa0ab2d284fca651f8d7621bc
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2023-04-05 18:35:23 -0500

    Import solution code

---
+++ app.py
@@ -0,0 +1,175 @@
+import re
+import numpy as np
+import pandas as pd
+
+# nltk
+import nltk
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+# sklearn
+from sklearn.naive_bayes import BernoulliNB
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix, classification_report
+
+from flask import Flask, request, jsonify, render_template
+
+#create Flask app
+app = Flask(__name__)
+
+
+# IMPORT DATASET
+# Importing the dataset
+DATASET_COLUMNS  = ["sentiment", "ids", "date", "flag", "user", "text"]
+DATASET_ENCODING = "ISO-8859-1"
+dataset = pd.read_csv('./reduced_dataset.csv',
+                      encoding=DATASET_ENCODING , names=DATASET_COLUMNS)
+
+# Reducing size of dataset to decrease runtime
+dataset = dataset.drop(range(0, len(dataset.index), 2))
+# Removing the unnecessary columns.
+dataset = dataset[['sentiment','text']]
+# Replacing the values to ease understanding.
+dataset['sentiment'] = dataset['sentiment'].replace(4,1)
+
+# Plotting the distribution for dataset.
+ax = dataset.groupby('sentiment').count().plot(kind='bar', title='Distribution of data',
+                                               legend=False)
+ax.set_xticklabels(['Negative','Positive'], rotation=0)
+
+# Storing data in lists.
+text, sentiment = list(dataset['text']), list(dataset['sentiment'])
+
+
+# TEXT PREPROCESSING
+
+# Define emojis and stopwords in English.
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed', 
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink', 
+          ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel','(:-D': 'gossip', '=^.^=': 'cat'}
+
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from', 
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're',
+             's', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+
+def preprocess(textdata):
+    processedText = []
+    
+    # Create Lemmatizer and Stemmer.
+    wordLemm = nltk.WordNetLemmatizer()
+    
+    # Defining regex patterns.
+    urlPattern        = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    userPattern       = '@[^\s]+'
+    alphaPattern      = "[^a-zA-Z0-9]"
+    sequencePattern   = r"(.)\1\1+"
+    seqReplacePattern = r"\1\1"
+    
+    for tweet in textdata:
+        tweet = tweet.lower()
+        
+        # Replace all URls with 'URL'
+        tweet = re.sub(urlPattern,' URL',tweet)
+        # Replace all emojis.
+        for emoji in emojis.keys():
+            tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])        
+        # Replace @USERNAME to 'USER'.
+        tweet = re.sub(userPattern,' USER', tweet)        
+        # Replace all non alphabets.
+        tweet = re.sub(alphaPattern, " ", tweet)
+        # Replace 3 or more consecutive letters by 2 letter.
+        tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
+
+        tweetwords = ''
+        for word in tweet.split():
+            # Checking if the word is a stopword.
+            #if word not in stopwordlist:
+            if len(word)>1:
+                # Lemmatizing the word.
+                word = wordLemm.lemmatize(word)
+                tweetwords += (word+' ')
+            
+        processedText.append(tweetwords)
+        
+    return processedText
+
+print("Preprocessing...")
+processedtext = preprocess(text)
+print("Preprocessing complete.")
+
+
+# SPLIT TRAINING & TEST DATASETS
+
+X_train, X_test, y_train, y_test = train_test_split(processedtext, sentiment, test_size = 0.05)
+print(f'Data Split done.')
+
+print("Fitting vectoriser...")
+vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
+vectoriser.fit(X_train)
+print(f'Vectoriser fitted.')
+print('No. of feature_words: ', len(vectoriser.get_feature_names()))
+
+print("Transforming data...")
+X_train = vectoriser.transform(X_train)
+X_test  = vectoriser.transform(X_test)
+print(f'Data Transformed.')
+
+# Fit model
+model = BernoulliNB(alpha = 2)
+model.fit(X_train, y_train)
+
+
+# Flask API Route
+@app.route("/", methods = ["GET","POST"])
+def index():
+    # GET webpage upon initial load
+    if request.method == "GET":
+        return render_template("index.html")
+    
+    # Form with searchtext is POSTed to the / path
+    if request.method == "POST":
+        print(request)
+        searchtext = request.form['searchterm']
+
+        # searchtext needs to be converted to a list as the model takes a list of strings as input
+        searchtext = [searchtext]
+        print(searchtext)
+
+        textdata = vectoriser.transform(preprocess(searchtext))
+        sentiment = model.predict(textdata)
+        # sentiment is retured as a list, either [1] or [0]
+        if sentiment[0] == 1:
+            sentimenttext = "Positive"
+        else:
+            sentimenttext = "Negative"
+        #find ways to call the model here! and return the output
+        return render_template("index.html", sentimenttext=sentimenttext)
+
+# New Route to About Page
+@app.route("/about", methods = ["GET"])
+def about():
+    # GET webpage upon initial load
+    if request.method == "GET":
+        return render_template("about.html")
+    
+if __name__ == "__main__":
+  app.run(debug=True, use_reloader=True)
---
+++ model.py
@@ -0,0 +1,175 @@
+# IMPORT LIBRARIES
+# utilities
+import re
+import numpy as np
+import pandas as pd
+import pickle
+
+# nltk
+import nltk
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+# sklearn
+from sklearn.naive_bayes import BernoulliNB
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+# IMPORT DATASET
+# Importing the dataset
+DATASET_COLUMNS  = ["sentiment", "ids", "date", "flag", "user", "text"]
+DATASET_ENCODING = "ISO-8859-1"
+dataset = pd.read_csv('./reduced_dataset.csv',
+                      encoding=DATASET_ENCODING , names=DATASET_COLUMNS)
+
+# Removing the unnecessary columns.
+dataset = dataset[['sentiment','text']]
+# Replacing the values to ease understanding.
+dataset['sentiment'] = dataset['sentiment'].replace(4,1)
+
+# Plotting the distribution for dataset.
+ax = dataset.groupby('sentiment').count().plot(kind='bar', title='Distribution of data',
+                                               legend=False)
+ax.set_xticklabels(['Negative','Positive'], rotation=0)
+
+# Storing data in lists.
+text, sentiment = list(dataset['text']), list(dataset['sentiment'])
+
+
+# TEXT PREPROCESSING
+
+# Define emojis and stopwords in English.
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed', 
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink', 
+          ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel','(:-D': 'gossip', '=^.^=': 'cat'}
+
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from', 
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're',
+             's', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+
+def preprocess(textdata):
+    processedText = []
+    
+    # Create Lemmatizer and Stemmer.
+    wordLemm = nltk.WordNetLemmatizer()
+    
+    # Defining regex patterns.
+    urlPattern        = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    userPattern       = '@[^\s]+'
+    alphaPattern      = "[^a-zA-Z0-9]"
+    sequencePattern   = r"(.)\1\1+"
+    seqReplacePattern = r"\1\1"
+    
+    for tweet in textdata:
+        tweet = tweet.lower()
+        
+        # Replace all URls with 'URL'
+        tweet = re.sub(urlPattern,' URL',tweet)
+        # Replace all emojis.
+        for emoji in emojis.keys():
+            tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])        
+        # Replace @USERNAME to 'USER'.
+        tweet = re.sub(userPattern,' USER', tweet)        
+        # Replace all non alphabets.
+        tweet = re.sub(alphaPattern, " ", tweet)
+        # Replace 3 or more consecutive letters by 2 letter.
+        tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
+
+        tweetwords = ''
+        for word in tweet.split():
+            # Checking if the word is a stopword.
+            #if word not in stopwordlist:
+            if len(word)>1:
+                # Lemmatizing the word.
+                word = wordLemm.lemmatize(word)
+                tweetwords += (word+' ')
+            
+        processedText.append(tweetwords)
+        
+    return processedText
+
+print("Preprocessing...")
+processedtext = preprocess(text)
+print("Preprocessing complete.")
+
+
+# SPLIT TRAINING & TEST DATASETS
+
+X_train, X_test, y_train, y_test = train_test_split(processedtext, sentiment, test_size = 0.05)
+print(f'Data Split done.')
+
+print("Fitting vectoriser...")
+vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
+vectoriser.fit(X_train)
+print(f'Vectoriser fitted.')
+print('No. of feature_words: ', len(vectoriser.get_feature_names()))
+
+print("Transforming data...")
+X_train = vectoriser.transform(X_train)
+X_test  = vectoriser.transform(X_test)
+print(f'Data Transformed.')
+
+
+# EVALUATE MODEL
+
+# def model_Evaluate(model):
+    
+#     # Predict values for Test dataset
+#     y_pred = model.predict(X_test)
+
+#     # Print the evaluation metrics for the dataset.
+#     print(classification_report(y_test, y_pred))
+
+
+BNBmodel = BernoulliNB(alpha = 2)
+BNBmodel.fit(X_train, y_train)
+
+pickle.dump(BNBmodel, open ("model.pkl", "wb"))
+
+# model_Evaluate(BNBmodel)
+
+
+
+# USE MODEL FOR PREDICTIONS
+
+def analyse_sentiment(text):
+    # Text to classify should be in a list.
+    
+    textdata = vectoriser.transform(preprocess(text))
+    sentiment = BNBmodel.predict(textdata)
+    
+    # Make a list of text with sentiment.
+    data = []
+    for text, pred in zip(text, sentiment):
+        data.append((text,pred))
+        
+    # Convert the list into a Pandas DataFrame.
+    df = pd.DataFrame(data, columns = ['text','sentiment'])
+    df = df.replace([0,1], ["Negative","Positive"])
+
+    print(df)
+
+
+# Enter your text below
+# Note: Text to classify should be in a list
+analyse_sentiment(["I hate you"])
